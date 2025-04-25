98f7a47fd54c5b95ba4b4a43b00af1e2fb1fee2f
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2025-04-25 13:50:41 +0200

    Drawer

--- app/src/main/java/com/example/mapsapp/ui/screens/DrawerScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/DrawerScreen.kt
@@ -1,18 +1,20 @@
 package com.example.mapsapp.ui.screens
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.compose.rememberNavController
 import com.example.mapsapp.ui.navigation.Destination
@@ -34,6 +37,7 @@ enum class DrawerItem(
     LIST(Icons.AutoMirrored.Filled.List, "Marker List", Destination.List)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DrawerScreen() {
     val navController = rememberNavController()
@@ -58,7 +62,7 @@ fun DrawerScreen() {
             }
         },
         drawerState = drawerState
-    ){/*
+    ){
         Scaffold(
             topBar = {
                 TopAppBar(
@@ -71,7 +75,7 @@ fun DrawerScreen() {
                 )
             }
         ) { innerPadding ->
-            NavigationWrapper(navController, Modifier.padding(innerPadding))
-        }*/
+            InternalNavigationWrapper(navController, Modifier.padding(innerPadding))
+        }
     }
 }

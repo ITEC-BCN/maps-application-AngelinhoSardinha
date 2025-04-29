93df53516c33c2fb5183ee8dae3bdfd6b36a704f
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2025-04-29 13:00:14 +0200

    Poco a Poco V3

--- app/src/main/java/com/example/mapsapp/ui/screens/CameraScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/CameraScreen.kt
@@ -12,13 +12,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,71 +39,84 @@ fun CameraScreen() {
     val context = LocalContext.current
     val imageUri = remember { mutableStateOf<Uri?>(null) }
     val bitmap = remember { mutableStateOf<Bitmap?>(null) }
-    //var showDialog by remember { mutableStateOf(false) }
 
-    /*
-    * val pickImageLauncher =
-   rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-       uri?.let {
-           imageUri.value = it
-           val stream = context.contentResolver.openInputStream(it)
-           bitmap.value = BitmapFactory.decodeStream(stream)
-       }
-   }
-   *
-   * if (showDialog) {
-   AlertDialog(onDismissRequest = { showDialog = false }, title = { Text("Selecciona una opción") },
-       text = { Text("¿Quieres tomar una foto o elegir una desde la galería?") },
-       confirmButton = {TextButton(onClick = {
-               showDialog = false
-               val uri = createImageUri(context)
-               imageUri.value = uri
-               takePictureLauncher.launch(uri!!)
-           }) { Text("Tomar Foto") }
-       },
-       dismissButton = {TextButton(onClick = {
-               showDialog = false
-               pickImageLauncher.launch("image/*")
-           }) { Text("Elegir de Galería") }
-       }
-   )
-}
-
-* Button(onClick = { showDialog = true }) {
-   Text("Abrir Cámara o Galería")
-}
-     */
-*/
-
-    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { success ->
-        if (success && imageUri.value != null) {
-            val stream = context.contentResolver.openInputStream(imageUri.value!!)
-            bitmap.value = BitmapFactory.decodeStream(stream)
+    val takePictureLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success && imageUri.value != null) {
+                val stream = context.contentResolver.openInputStream(imageUri.value!!)
+                bitmap.value = BitmapFactory.decodeStream(stream)
+            }
         }
+
+    val pickImageLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                imageUri.value = it
+                val stream = context.contentResolver.openInputStream(it)
+                bitmap.value = BitmapFactory.decodeStream(stream)
+            }
+        }
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("Selecciona una opción") },
+            text = { Text("¿Quieres tomar una foto o elegir una desde la galería?") },
+            confirmButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                    val uri = createImageUri(context)
+                    imageUri.value = uri
+                    takePictureLauncher.launch(uri!!)
+                }) {
+                    Text("Tomar Foto")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                    pickImageLauncher.launch("image/*")
+                }) {
+                    Text("Elegir de Galería")
+                }
+            }
+        )
     }
 
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = {
-            val uri = createImageUri(context)
-            imageUri.value = uri
-            launcher.launch(uri!!)
-        }) {
-            Text("Abrir Cámara")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { showDialog = true }) {
+            Text("Abrir Cámara o Galería")
         }
+
         Spacer(modifier = Modifier.height(24.dp))
+
         bitmap.value?.let {
-            Image(bitmap = it.asImageBitmap(), contentDescription = null,
-                modifier = Modifier.size(300.dp).clip(RoundedCornerShape(12.dp)),contentScale = ContentScale.Crop)
+            Image(
+                bitmap = it.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(300.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop
+            )
         }
     }
 }
 
-fun createImageUri(context :Context): Uri? {
+fun createImageUri(context: Context): Uri? {
     val file = File.createTempFile("temp_image_", ".jpg", context.cacheDir).apply {
         createNewFile()
         deleteOnExit()
     }
+
     return FileProvider.getUriForFile(
         context,
         "${context.packageName}.fileprovider",

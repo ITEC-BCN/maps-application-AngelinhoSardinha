023dc2013ee520eff055d7100c586276b74603af
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2025-04-29 13:02:33 +0200

    Poco a Poco V4

--- app/src/main/java/com/example/mapsapp/ui/screens/CreateMarkerScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/CreateMarkerScreen.kt
@@ -1,15 +1,25 @@
 package com.example.mapsapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun CreateMarkerScreen(coordenadas: String, modifier: Modifier, onMapClick: (LatLng) -> Unit) {
+fun CreateMarkerScreen(coordenadas: String, modifier: Modifier, navigateToMarker: () -> Unit) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Text(text = "Crear Marcador", style = MaterialTheme.typography.headlineLarge)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Coordenadas: $coordenadas")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = navigateToMarker) {
+            Text(text = "Guardar Marcador")
+        }
+    }
 }

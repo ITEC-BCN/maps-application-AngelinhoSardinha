65fdf0446d222486d6ceab16830d1d903ff8770e
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2025-05-09 13:40:23 +0200

    Perfect

--- app/src/main/java/com/example/mapsapp/ui/screens/MarkerListScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/MarkerListScreen.kt
@@ -6,17 +6,57 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.rememberCameraPositionState
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mapsapp.data.model.Marker
+import com.example.supabasetest.viewmodel.MyViewModel
 
 @Composable
-fun MarkerListScreen() {
-    Column(modifier = Modifier.background(color = Color.Black)) {
-        Text("HOLA")
+fun MarkerListScreen(onMarkerClick: (Marker) -> Unit = {}) {
+    val viewModel = viewModel<MyViewModel>()
+    val markers by viewModel.markersList.observeAsState(emptyList())
+
+    // Obtener los marcadores al cargar la pantalla
+    LaunchedEffect(Unit) {
+        viewModel.getAllMarkers()
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            "Lista de Marcadores",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(bottom = 16.dp)
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 75.dp) // Agrega un padding superior para evitar que el primer ítem se corte
+        ) {
+            items(markers) { marker ->
+                MarkerItem(marker = marker) {
+                    onMarkerClick(marker)
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mapsapp/ui/screens/MarkersScreen.kt
+++ app/src/main/java/com/example/mapsapp/ui/screens/MarkersScreen.kt
@@ -1,13 +1,16 @@
 package com.example.mapsapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -31,10 +34,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.example.mapsapp.data.model.Marker
 
 @Composable
@@ -124,6 +129,15 @@ fun MarkerItem(marker: Marker, onClick: () -> Unit) {
             .padding(8.dp)
     ) {
         Column {
+            Image(
+                painter = rememberAsyncImagePainter(marker.image),
+                contentDescription = "Imagen del marcador",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(8.dp))
             Text("Name: ${marker.name}", fontSize = 20.sp, fontWeight = FontWeight.Bold)
             Text("Coordinates: ${marker.latlng ?: "N/A"}")
             Text("Description: ${marker.description}")
